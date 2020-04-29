27231054ade4d70d0ff195bd43abe7f8a0c225a0
ruchika <ruchika6698@gmail.com>
2020-04-29 13:31:50 +0530

    Added:Welcome to car service generator

---
+++ CarService/Invoicegenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CarService
+{
+    public class Invoicegenerator
+    {
+        static void Main(string[] args)
+        {
+            /// <summary>
+            /// Main Method
+            /// </summary>
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Welcome to subscription based car service");
+            }
+        }
+    }
+}

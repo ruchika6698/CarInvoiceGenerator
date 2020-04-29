c042464d08523926763d6bb9462a900dfd887be7
ruchika <ruchika6698@gmail.com>
2020-04-29 14:31:22 +0530

    Added:step2:Calculate aggregate of totalfare for multiple rides

--- TestCarService/UnitTest1.cs
+++ TestCarService/UnitTest1.cs
@@ -1,18 +1,33 @@
+using CarService;
 using NUnit.Framework;
 
 namespace TestCarService
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        Invoicegenerator invoicegenerator = new Invoicegenerator();
+        /// <summary>
+        /// UC-1: Test case to calculate fare for journey
+        /// </summary>
+        [Test]
+        public void GivenDistanceAndTime_whenInvoiceGeneratorMust_ReturntotalFareForJourney()
         {
+            double total = invoicegenerator.TotalFare(15, 20);
+            Assert.AreEqual(170, total);
         }
 
+        /// <summary>
+        /// UC-2: Test case to calculate aggregate fare for multiple Rides
+        /// </summary>
         [Test]
-        public void Test1()
+        public void GivenDistanceAndTime_whenTakeMultipleRides_AndCalculateAggregateTotal()
         {
-            Assert.Pass();
+            Ride[] ride = {
+                new Ride(50,20),
+                new Ride(10,30)
+            };
+            double total = invoicegenerator.TotalFare(ride);
+            Assert.AreEqual(325, total);
         }
     }
 }

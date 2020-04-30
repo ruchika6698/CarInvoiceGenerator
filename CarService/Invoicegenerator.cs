18ba5ce7ad525f1f8c7df89ab333005636f844d4
ruchika <ruchika6698@gmail.com>
2020-04-30 16:30:37 +0530

    Added:step4:Get list of rides from rideReopsitory

--- CarService/Invoicegenerator.cs
+++ CarService/Invoicegenerator.cs
@@ -4,15 +4,64 @@ namespace CarService
 {
     public class Invoicegenerator
     {
+        /// <summary>
+        /// constants values for costperkm,costpermin,minimumfare
+        /// </summary>
+        public int costPerkilometer = 10;
+        public int costPerMinute = 1;
+        public int minimumFare = 5;
+        public double totalFare = 0;
+        public int totalRides = 0;
+        public double aggregateFare = 0;
+
+        /// <summary>
+        /// return aggregate value
+        /// </summary>
+        public double Aggregate
+        {
+            get
+            {
+                return this.aggregateFare;
+            }
+        }
+
+        /// <summary>
+        /// Main Method
+        /// </summary>
         static void Main(string[] args)
         {
-            /// <summary>
-            /// Main Method
-            /// </summary>
-            static void Main(string[] args)
+            Console.WriteLine("Welcome to subscription based car service");
+        }
+
+        /// <summary>
+        /// Method to calculate total fare of car
+        /// </summary>
+        public double TotalFare(double distance, double time)
+        {
+            //calculate Total Fare
+            double totalFare = (distance * costPerkilometer) + (time * costPerMinute);
+            //if the totalfare is greater than minimum fare then return totalfare
+            if (totalFare > minimumFare)
             {
-                Console.WriteLine("Welcome to subscription based car service");
+                return totalFare;
             }
+            //if the totalfare is less than minimum fare then return totalfare
+            return minimumFare;
+        }
+        /// <summary>
+        /// Method to calculate total fare of car with multiple rides
+        /// </summary>
+        public double CalculateMonthlyFare(Ride[] rides)
+        {
+            //calculate Total Fare for multipme rides
+            foreach (var total in rides)
+            {
+                totalFare = totalFare + TotalFare(total.distance, total.time);
+            }
+            totalRides = rides.Length;
+            //calculate aggregate of total fare
+            aggregateFare = totalFare / totalRides;
+            return totalFare;
         }
     }
 }
---
+++ CarService/Ride.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarService
+{
+    public class Ride
+    {
+        public double distance;
+        public double time;
+        /// <summary>
+        /// constructor for Ride class
+        /// </summary>
+        public Ride(double distance, double time)
+        {
+            this.distance = distance;
+            this.time = time;
+        }
+    }
+}
